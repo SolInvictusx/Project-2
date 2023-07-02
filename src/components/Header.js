e12b24f5c30206d268dd136601fb225211806dff
SolInvictusx <ambrosi52@live.com>
2023-07-01 19:22:11 -0500

    campsite slice

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import { Navbar, NavbarBrand } from 'reactstrap';
+import NuCampLogo from '../app/assets/img/logo.png';
+
+const Header = () => {
+    return (
+        <Navbar dark color='primary' sticky='top' expand='md'>
+
+        </Navbar>
+    )
+}
+
+
+export default Header;
