73db0ba3bbaa90bee9f2b001447d8d72d7bec696
SolInvictusx <ambrosi52@live.com>
2023-07-12 23:19:25 -0500

    Changed about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -29,9 +29,9 @@ const AboutPage = () => {
                             <dl className='row'>
                                 <dt className='col-6'>Founded</dt>
                                 <dd className='col-6'>February 3, 2016</dd>
-                                <dt className='col-6'>No. of Campsites in 2019</dt>
+                                <dt className='col-6'>No. of Campsites in 2022</dt>
                                 <dd className='col-6'>563</dd>
-                                <dt className='col-6'>No. of Reviews in 2019</dt>
+                                <dt className='col-6'>No. of Reviews in 2022</dt>
                                 <dd className='col-6'>4388</dd>
                                 <dt className='col-6'>Employees</dt>
                                 <dd className='col-6'>42</dd>
